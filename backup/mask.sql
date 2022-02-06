b79ba58168d43abc0ab41fb2eefa06dad727a73b
Mário Santos Sousa <m_santos_sousa@hotmail.com>
2022-02-06 14:27:24 +0000

    Create mask.sql

---
+++ backup/mask.sql
@@ -0,0 +1,5 @@
+update wpq7_options set option_value = 'http://localhost/ecordel' where option_id = 1;
+update wpq7_options set option_value = 'http://localhost/ecordel' where option_id = 2;
+
+-- admin
+update wpq7_users SET user_pass='21232f297a57a5a743894a0e4a801fc3';
